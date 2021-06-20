f1e5ed04326b11bfeda11b5d8e78bc2144eac812
ducquang2 <quangbilun@gmail.com>
2021-06-20 19:20:29 +0700

    design menu

---
+++ Course-Registration-System/menu.cpp
@@ -0,0 +1,70 @@
+#include "menu.h"
+
+void showMenu()
+{
+	system("cls");
+	cout << "|------------------------|" << endl;
+	cout << "|        MAIN MENU       |" << endl;
+	cout << "|------------------------|" << endl;
+	cout << "| No |      Option       |" << endl;
+	cout << "|------------------------|" << endl;
+	cout << "| 1  | Log in            |" << endl;
+	cout << "|------------------------|" << endl;
+	cout << "| 2  | Exit              |" << endl;
+	cout << "|------------------------|" << endl;
+}
+
+void showMenuOfStudent()
+{
+	system("cls");
+
+	cout << "|--------------------------------------|" << endl;
+	cout << "|                STUDENT               |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| No |             Option              |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 1  | Enroll                          |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 2  | View enrolling result           |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 3  | Remove course                   |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 4  | View his/her scores of a course |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 5  | View profile info               |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 6  | Change password                 |" << endl;
+	cout << "|--------------------------------------|" << endl;
+	cout << "| 7  | Log out                         |" << endl;
+	cout << "|--------------------------------------|" << endl;
+
+}
+
+void showMenuOfStaff()
+{
+	system("cls");
+	cout << "|------------------------------------------------|" << endl;
+	cout << "|                     STAFF                      |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| No |      Option                               |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 1  | Class                                     |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 2  | Create academic year and semester         |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 3  | Enter existing academic year and semester |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 4  | Scoreboards                               |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 5  | Attendance list                           |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 6  | For input of lecturer                     |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 7  | View profile info                         |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 8  | Change password                           |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+	cout << "| 9  | Log out                                   |" << endl;
+	cout << "|------------------------------------------------|" << endl;
+
+}
---
+++ Course-Registration-System/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H_
+#define MENU_H_
+
+
+#include<iostream>
+using namespace std;
+
+void showMenu();
+void showMenuOfStudent();
+void showMenuOfStaff();
+
+#endif
