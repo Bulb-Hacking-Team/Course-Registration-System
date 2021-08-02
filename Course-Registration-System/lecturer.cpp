4c6f8bbed862fed214095addfbd7de4ac65b6cd7
ducquang2 <nducquangg@gmail.com>
2021-08-02 20:07:36 +0700

    compare lecturer and alloc list

--- Course-Registration-System/lecturer.cpp
+++ Course-Registration-System/lecturer.cpp
@@ -1,1 +1,19 @@
 #include "leturer.h"
+
+bool isEqualLecturer(void* val1, void* val2)
+{
+	Lecturer* lec1, * lec2;
+	lec1 = (Lecturer*)val1;
+	lec2 = (Lecturer*)val2;
+
+	if (lec1->info.acc.username == lec2->info.acc.username)
+		return true;
+
+	return false;
+}
+
+void* allocListLecturers(const int& countLecturer)
+{
+	Lecturer* arr = new Lecturer[countLecturer];
+	return arr;
+}
--- Course-Registration-System/leturer.h
+++ Course-Registration-System/leturer.h
@@ -1,3 +1,7 @@
 #pragma once
 #include "student.h"
 
+bool isEqualLecturer(void* val1, void* val2);
+
+
+void* allocListLecturers(const int& countLecturer);
